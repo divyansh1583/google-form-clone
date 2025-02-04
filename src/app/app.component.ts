be32cc8972397b9c45cda20e34ceb8e7e80b9efe
Divyansh Chauhan <divyanshchauhan28@gmail.com>
2025-02-04 22:54:49 +0530

    feat: implement Angular Material Sidenav and enhance form creation layout with Material Design components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatSidenavModule],
   template: `
-      <router-outlet></router-outlet>
-  `
+  <mat-sidenav-container>
+  <mat-sidenav-content>
+    <router-outlet></router-outlet>
+  </mat-sidenav-content>
+</mat-sidenav-container>
+
+  `,
+  styles: [`main{
+    height: 100vh;
+    width: 100%;
+  }`]
 })
 export class AppComponent {}
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,34 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./core/auth/auth.component').then(m => m.AuthComponent),
-        children: [
-            {
-                path: 'login',
-                loadComponent: () => import('./core/auth/login/login.component').then(m => m.LoginComponent)
-            },
-            {
-                path: 'register',
-                loadComponent: () => import('./core/auth/register/register.component').then(m => m.RegisterComponent)
-            },
-            {
-                path: 'forgot-password',
-                loadComponent: () => import('./core/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
-            },
-        ]
-    },
-    {
-        path: '',
-        loadComponent: () => import('./core/layout/layout.component').then(m => m.LayoutComponent),
-        children: [
-            {
-                path: 'dashboard',
-                loadComponent: () => import('./features/dashboard/pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
-            },
-        ]
-    },
+    // {
+    //     path: '',
+    //     loadComponent: () => import('./core/auth/auth.component').then(m => m.AuthComponent),
+    //     children: [
+    //         {
+    //             path: 'login',
+    //             loadComponent: () => import('./core/auth/login/login.component').then(m => m.LoginComponent)
+    //         },
+    //         {
+    //             path: 'register',
+    //             loadComponent: () => import('./core/auth/register/register.component').then(m => m.RegisterComponent)
+    //         },
+    //         {
+    //             path: 'forgot-password',
+    //             loadComponent: () => import('./core/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+    //         },
+    //     ]
+    // },
+    // {
+    //     path: '',
+    //     loadComponent: () => import('./core/layout/layout.component').then(m => m.LayoutComponent),
+    //     children: [
+    //         {
+    //             path: 'dashboard',
+    //             loadComponent: () => import('./features/dashboard/pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    //         },
+    //     ]
+    // },
     {
         path: 'forms',
         loadComponent: () => import('./features/form/form.component').then(m => m.FormComponent),
